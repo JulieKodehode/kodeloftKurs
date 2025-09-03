a5af178516d92d73af916a11b786a42f4c6477f6
JulieKodehode <julie.kodehode@gmail.com>
2025-09-03 13:31:13 +0200

    Ryddet opp i kommentarer og gjort de Norske

--- js/languageToggle.js
+++ js/languageToggle.js
@@ -3,23 +3,20 @@ const languageCardDemoColor = document.querySelector("#languageCardDemoColor");
 function languageToggle() {
     console.log("Clicked?");
 
-    // Always set the transition once so it applies to changes
+    // Sett opp overgang koden en gang for begge betingelsene
     languageCardDemoColor.style.transition = "color 3s ease, flex-direction 0.5s ease";
+
     if (!languageCardDemoColor.classList.contains("languageCardDemoCss")) {
         languageCardDemoColor.classList.add("languageCardDemoCss");
 
-        // animate text color
-        languageCardDemoColor.style.color = "#f3a366"; /* Changed color */
+        languageCardDemoColor.style.color = "#f3a366"; // Fargen som endrer seg
 
-        // animate direction
         languageCardDemoColor.style.flexDirection = "column-reverse";
     } else {
         languageCardDemoColor.classList.remove("languageCardDemoCss");
 
-        // animate text color
-        languageCardDemoColor.style.color = "black"; /* Default text color */
+        languageCardDemoColor.style.color = "inherit"; // Default fargen ut i fra fargetema
 
-        // animate direction
         languageCardDemoColor.style.flexDirection = "column";
     }
 }
--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+// Modelær CSS oppsett som knytter flere filer til en hoved fil
 import { themeToggle } from "./js/themeToggle.js";
 import { hamburgerToggle } from "./js/hamburgerToggle.js";
 import { learningToggle } from "./js/learningToggle.js";
